16d9a1beca825f24449013628d13fca670bc1f47
princess-mikus <kiankocasaubon01@gmail.com>
2024-09-17 13:22:02 +0200

    ex00 refined and Makefile done

--- cpp-00/ex00/megaphone.cpp
+++ cpp-00/ex00/megaphone.cpp
@@ -1,37 +1,36 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fcasaubo <fcasaubo@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/12 13:32:10 by fcasaubo          #+#    #+#             */
-/*   Updated: 2024/06/12 14:03:09 by fcasaubo         ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   megaphone.cpp									  :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: fcasaubo <fcasaubo@student.42.fr>		  +#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2024/06/12 13:32:10 by fcasaubo		  #+#	#+#			 */
+/*   Updated: 2024/09/17 13:15:18 by fcasaubo		 ###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 #include <iostream>
 
-void    inter(char* str, int funciont(char c))
+std::string	str_to_mayus(char *str_org)
 {
-    for (size_t i = 0; str[i]; i++)
-       str[i] = funciont(str[i]);
-    
-}
-
-std::string    str_to_mayus(char *str_org)
-{
-    std::string str = str_org;
-    for (size_t i = 0; i < str.length(); i++)
-       str_org[i] = toupper(str_org[i]);
-    return (str);
+	std::string str = str_org;
+	for (size_t i = 0; i < str.length(); i++)
+	   str[i] = toupper(str_org[i]);
+	return (str);
 }
 
 
 int main(int argc, char **argv) {
-    (void)argc;
-    std::string str;
-    str = str_to_mayus(argv[1]);
-    std::cout << argv[1] << '\n' << str << '\n';
-    return 0;
+	std::string str;
+	if (argc < 2)
+		return (0);
+	for (int i = 1; i < argc; i++) {
+		str = str_to_mayus(argv[i]);
+		std::cout << str;
+		if (i != argc - 1)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+	return 0;
 }
