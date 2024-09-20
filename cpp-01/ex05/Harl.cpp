a5d2a0c98c7a4e9769c5d46087f458af48ca3738
princess-mikus <kiankocasaubon01@gmail.com>
2024-09-20 17:57:53 +0200

    I insist in the Sam & Max quotes, actually cool stuff

--- cpp-01/ex05/Harl.cpp
+++ cpp-01/ex05/Harl.cpp
@@ -19,5 +19,17 @@ void	Harl::error() {
 }
 
 void	Harl::complain(std::string level) {
-	
+	void (Harl::*function_list[])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string	level_list[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	for (size_t i = 0; i < 5; i++)
+	{
+		if (level == level_list[i])
+		{
+			(this->*function_list[i])();
+			break;
+		}
+		if (i == 4)
+			std::cout << "I could do this all day, and I intend to! (Level not found)" << std::endl;
+	}
 }
---
+++ cpp-01/ex05/main.cpp
@@ -0,0 +1,12 @@
+#include "Harl.hpp"
+
+int	main(void)
+{
+	Harl	Max;
+
+	Max.complain("DEBUG");
+	Max.complain("INFO");
+	Max.complain("WARNING");
+	Max.complain("ERROR");
+	Max.complain("Wathever");
+}
