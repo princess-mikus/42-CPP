ed68e0d17f9c76d5f11fc659831bf71fb62416d0
princess-mikus <kiankocasaubon01@gmail.com>
2024-09-22 20:48:08 +0200

    I don't know what I changed really but pushing anyway!

---
+++ cpp-01/ex06/Harl.hpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+
+#ifndef HARL_HPP
+#define HARL_HPP
+class Harl {
+	public:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+		void	complain(std::string level);
+};
+#endif
