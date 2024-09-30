2974656e213520f392d3d0b304e64211b2c0d7ce
princess-mikus <kiankocasaubon01@gmail.com>
2024-09-30 12:22:01 +0200

    Makefiles for CPP-01

--- cpp-01/ex01/main.cpp
+++ cpp-01/ex01/main.cpp
@@ -1,10 +1,8 @@
 #include "Zombie.hpp"
 
-int	main(int argc, char **argv) {
-	if (argc != 3 || atoi(argv[1]) <= 0)
-		return (2);
-	int	zombNum = atoi(argv[1]);
-	std::string zombName = argv[2];
+int	main(void) {
+	int	zombNum = 3;
+	std::string zombName = "Pedro";
 	Zombie	*Horde = zombieHorde(zombNum, zombName);
 	for (int i = 0; i < zombNum; i++)
 		Horde[i].announce();
